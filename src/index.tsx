a582ce29e3faf563929fc6e5c5465233cc76fae4
Andrii Trybynenko <tamerjlah@gmail.com>
2019-04-14 16:22:28 +0200

    shift2ts

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+class Index extends React.Component<object> {
+  render() {
+    return <div>Loader...</div>;
+  }
+}
+
+ReactDOM.render(<Index />, document.querySelector('#root'));
