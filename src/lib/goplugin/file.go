71c6d284e047cc046f28ddf8f6bf7c09b0645820
Andrii Trybynenko <tamerjlah@gmail.com>
2019-07-30 01:35:30 +0200

    parse big file

--- src/lib/goplugin/file.go
+++ src/lib/goplugin/file.go
@@ -6,12 +6,26 @@ import (
 )
 
 func main() {
+	const bytesLimit = 1500
+	var cursor = 0
 	fileName := "test.txt"
 	content, err := ioutil.ReadFile(fileName)
-	slice := content[0:1500]
 	checkError(err)
+	itemsCount := 56489 // len(content) // 564892290
+	slice := content[cursor:itemsCount]
 	result := string(slice)
-	fmt.Println("Read from file:", result)
+
+	for itemsCount > 0 {
+		itemsCount -= bytesLimit
+		cursor += bytesLimit
+
+		if itemsCount <= 0 || int(cursor) > int(itemsCount) {
+			return
+		}
+
+		slice := content[cursor:itemsCount]
+		fmt.Println("Read from file:", len(slice), itemsCount)
+	}
 }
 
 func checkError(err error) {
